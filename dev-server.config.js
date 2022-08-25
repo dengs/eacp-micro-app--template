3f08bf7376b8e1fbd9d395f865dba07c2b6e6d1c
dengs <ds@hanzhisoft.com>
2022-08-25 18:56:31 +0800

    增加EACP平台微前端基座模块：@hanzhisoft/eacp-admin & 服务运行配置

---
+++ dev-server.config.js
@@ -0,0 +1,28 @@
+const EacpAdminDevServerConfig = require("@hanzhisoft/eacp-admin/config/dev-server.config");
+const path = require("path");
+const { merge } = require("webpack-merge");
+
+/**
+ * eacp-admin 模块路径
+ */
+const EACP_ADMIN_MODULE_PATH = path.join(__dirname, "./node_modules/@hanzhisoft/eacp-admin");
+
+/**
+ * 合并配置 & 导出
+ */
+module.exports = merge(EacpAdminDevServerConfig, {
+  entry: path.join(EACP_ADMIN_MODULE_PATH, `/config/dev-app.js`),
+  devServer: {
+    static: {
+      directory: path.join(EACP_ADMIN_MODULE_PATH, `/dist`),
+    },
+    proxy: {
+      "/api": {
+        target: "http://192.168.70.216:10006",
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: { "^/api": "api" },
+      },
+    },
+  },
+});
