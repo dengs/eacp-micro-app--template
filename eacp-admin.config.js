1af927f613253bb25a87fb5abc445b2595d2b855
dengs <ds@hanzhisoft.com>
2022-08-26 10:44:47 +0800

    EACP平台微前端基座服务启动配置

--- eacp-admin.config.js
+++ eacp-admin.config.js
@@ -13,16 +13,17 @@ const EACP_ADMIN_MODULE_PATH = path.join(__dirname, "./node_modules/@hanzhisoft/
 module.exports = merge(EacpAdminDevServerConfig, {
   entry: path.join(EACP_ADMIN_MODULE_PATH, `/config/dev-app.js`),
   devServer: {
+    port: 9527,
     static: {
-      directory: path.join(EACP_ADMIN_MODULE_PATH, `/dist`),
+      directory: path.join(EACP_ADMIN_MODULE_PATH, `/dist`)
     },
     proxy: {
       "/api": {
         target: "http://192.168.70.216:10006",
         ws: false,
         changeOrigin: true,
-        pathRewrite: { "^/api": "api" },
-      },
-    },
-  },
+        pathRewrite: { "^/api": "api" }
+      }
+    }
+  }
 });
