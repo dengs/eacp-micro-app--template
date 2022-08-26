6b4d2760b270dd42de905892e039e6c71e7cbd3c
dengs <ds@hanzhisoft.com>
2022-08-26 10:43:16 +0800

    eslint 配置

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "warn", // 许变量定义未使用
+    "no-empty": "warn", // 不允许空代码块
+    "no-prototype-builtins": "off" // 不允许访问原型属性
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
