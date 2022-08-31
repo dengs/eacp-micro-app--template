51f0e3b8d47ea1c9c90e3ffa54b0f5bf56163c27
dengs <ds@hanzhisoft.com>
2022-08-31 15:03:06 +0800

    上下文对象扩展 env 信息

--- src/core/Context.js
+++ src/core/Context.js
@@ -5,6 +5,10 @@ const { app: mainApp, modules } = window.microApp.getData();
 const { $store: store, $i18n: i18n } = mainApp;
 const { Mixins, Utils } = modules;
 
-export { mainApp, store, i18n, modules, Mixins, Utils };
+const env = {
+  microAppName: process.env.VUE_APP_MICRO_APP_NAME
+};
 
-export default { mainApp, store, i18n, modules, Mixins, Utils };
+export { env, mainApp, store, i18n, modules, Mixins, Utils };
+
+export default { env, mainApp, store, i18n, modules, Mixins, Utils };
