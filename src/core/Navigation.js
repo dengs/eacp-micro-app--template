be75e1f3a0acf1c16e6cc3c11e2f03b5d73366c8
dengs <ds@hanzhisoft.com>
2022-09-01 09:50:49 +0800

    打开页签方法扩展

--- src/core/Navigation.js
+++ src/core/Navigation.js
@@ -1,7 +1,21 @@
 const { app: mainApp } = window.microApp.getData();
+const { microAppName } = require("./Context").env;
 
-export const openMicroAppTab = ({ key, title, bread, appKey, params = {} } = {}) => {
-  mainApp.openTab({ key, title, bread, appKey, params, isMicroApp: true });
-};
+/**
+ * 打开页签
+ * 注：isMicroApp = false 时需传入 component
+ * @param {*} options
+ * @returns
+ */
+export const openTab = ({ key, title, routePath, bread, appKey = microAppName, component, params, isMicroApp } = {}) =>
+  mainApp.openTab({ key, title, routePath, bread, appKey, component, params, isMicroApp });
 
-export default { openMicroAppTab };
+/**
+ * 打开页签—微应用页面
+ * @param {*} options
+ * @returns
+ */
+export const openMicroAppTab = ({ key, title, routePath, bread, appKey = microAppName, params } = {}) =>
+  mainApp.openMicroAppTab({ key, title, routePath, bread, appKey, params });
+
+export default { openTab, openMicroAppTab };
