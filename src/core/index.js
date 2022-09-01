55ded661db4874aad044878c3c0b609c1b74ed06
dengs <ds@hanzhisoft.com>
2022-09-01 09:50:32 +0800

    core 模块导出

--- src/core/index.js
+++ src/core/index.js
@@ -2,7 +2,8 @@ import BaseApi from "./BaseApi";
 import Mixins from "./Mixins";
 import Context from "./Context";
 import Components from "./Components";
+import Navigation from "./Navigation";
 
-export { BaseApi, Mixins, Context, Components };
+export { BaseApi, Mixins, Context, Components, Navigation };
 
-export default { BaseApi, Mixins, Context, Components };
+export default { BaseApi, Mixins, Context, Components, Navigation };
